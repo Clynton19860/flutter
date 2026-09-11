8d060956ce81fae1e85402a52b14da74556a6140
chevonnes-tih <chevonnes@tihsa.co.za>
2026-09-11 10:08:04 +0200

    Day 5: Signing (keystore intentionally omitted)

--- quote_app/android/app/build.gradle.kts
+++ quote_app/android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,6 +15,19 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword - keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
     namespace = "za.co.moint.quote_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
